fc8874ec4573d3e16a703e41026a56102393e453
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-11-12 17:12:22 +0700

    Fix styling not clipped

--- src/components/_pages/join/index.js
+++ src/components/_pages/join/index.js
@@ -1,6 +1,17 @@
-import { Typography, Container, Box, Button } from '@mui/material';
+import { Typography, Container, Box, Button, Grid } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import { useMemo } from 'react';
+
+import { styled } from '@mui/material/styles';
+
+const ClippedTypography = styled(Typography)(() => ({
+  wordWrap: 'break-word',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  display: '-webkit-box',
+  WebkitLineClamp: 4,  //max num of lines to show
+  WebkitBoxOrient: 'vertical'
+}));
 
 function useQuery() {
   const { search } = useLocation();
@@ -13,33 +24,36 @@ export default function JoinPage() {
   const classId = queryString.get('class_id');
   return(
     <Container maxWidth='md'>
-      <Box
+      <Grid
+        container
         display='flex'
         direction='column'
         alignItems='center'
         justifyContent='center'
         style={{ minHeight: '90vh' /* Layout height */}}
       >
-        <Box>
-          <Typography variant='h5'>
-            Bạn được mời tham gia lớp
-          </Typography>
-          <Typography variant='h4'>
-            <b>[Tên lớp]</b>
-          </Typography>
-          <Typography variant='subtitle1'>
-            <b>[Giáo viên/ Học sinh]</b>
-          </Typography>
-          <Button 
-            variant='contained'
-          >
-            Tham gia
-          </Button>
-          <Typography variant='subtitle2'>
-            invitation: {invitation} | class_id: {classId}
-          </Typography>
-        </Box>
-      </Box>
+        <Grid item xs={12}>
+          <Box maxWidth='300px'>
+            <Typography variant='body1'>
+              Bạn được mời tham gia lớp
+            </Typography>
+            <ClippedTypography variant='h6'>
+              <b>[Tên lớp]</b>
+            </ClippedTypography>
+            <Typography variant='subtitle1'>
+              <b>[Giáo viên/ Học sinh]</b>
+            </Typography>
+            <Button 
+              variant='contained'
+            >
+              Tham gia
+            </Button>
+            <Typography variant='subtitle2'>
+              invitation: {invitation} | class_id: {classId}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   )
 }

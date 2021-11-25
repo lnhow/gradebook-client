0ad8c73be74a6ccb9b28daa1cc916420e4f2fd00
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-11-25 18:04:26 +0700

    Refactor class context for easier reading

--- src/components/_pages/classroomSingle/context/currentClassContext.js
+++ src/components/_pages/classroomSingle/context/currentClassContext.js
@@ -1,76 +1,47 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { selectUser } from '../../../../redux/slices/user';
-
 import { USER_CLASS_ROLES } from '../../../../helpers/constants';
 
-const DEFAULT_USER_CLASS_ROLE = USER_CLASS_ROLES.STUDENT;
-
-const USER_INITIAL_STATE = {
-  user_id: '',
-  user_code: '',
-  full_name: '',
-  role: DEFAULT_USER_CLASS_ROLE,
-}
-
-const CLASS_INITIAL_STATE = {
-  class_id: '',
-  class_name: '',
-  subject: '',
-  description: '',
-  created_at: '',
-  updated_at: '',
-  status: '',
-  users: [],
-  owner: {
-    user_id: '',
-    avatar: '',
-    full_name: '',
-  }
-}
+import { 
+  CLASS_INITIAL_STATE, CLASS_ASSIGNMENTS_INITIAL_STATE, USER_INITIAL_STATE,
+  getClassInfo, getCurrentUserInfoInClass, getClassAssignments
+} from './helpers';
 
 export const CurrentClassContext = createContext({
   currentClass: CLASS_INITIAL_STATE,
+  setCurrentClass: () => {},  //EMPTY FUNCTION
+  classAssignments: CLASS_ASSIGNMENTS_INITIAL_STATE,
+  setClassAssignments: () => {},
   currentUser: USER_INITIAL_STATE,
   isTeacher: USER_INITIAL_STATE.role === USER_CLASS_ROLES.TEACHER  //Is current signed in user a teacher
 });
 
 export default function CurrentClassProvider({classroom_info = {}, children}) {
   const user = useSelector(selectUser);
-  const usersInClass = classroom_info.listUser || [];
+  const [currentClass, _setCurrentClass] = useState(getClassInfo(classroom_info));
+  const [classAssignments, _setClassAssignments] = useState(getClassAssignments(classroom_info));
+  const currentUserInClass = getCurrentUserInfoInClass(currentClass.users, user);
 
-  const currentUsersInClass = usersInClass.filter((singleUser) => singleUser.user_id === user.id)
-  let currentUserInClass = (currentUsersInClass.length > 0) ? currentUsersInClass[0] : USER_INITIAL_STATE;
-  
-  currentUserInClass = {
-    user_id: currentUserInClass.user_id,
-    user_code: currentUserInClass.user_code || '',
-    full_name: currentUserInClass.full_name || '',
-    role: currentUserInClass.role || DEFAULT_USER_CLASS_ROLE,
+  const setCurrentClass = (classroom_info) => {
+    _setCurrentClass(getClassInfo(classroom_info));
   }
 
-  let currentClass = {
-    class_id: classroom_info.id || '',
-    class_name: classroom_info.class_name || '',
-    subject: classroom_info.subject || '',
-    description: classroom_info.description || '',
-    created_at: classroom_info.created_at || '',
-    updated_at: classroom_info.updated_at || '',
-    status: classroom_info.status || '',
-    users: usersInClass,
-    owner: {
-      user_id: classroom_info.owner_id || '',
-      avatar: classroom_info.owner_avatar || '',
-      full_name: classroom_info.owner_name || '',
-    }
+  const setClassAssignments = (newClassAssignments = []) => {
+    _setClassAssignments(newClassAssignments);
   }
 
   const contextValue = {
     currentClass: currentClass,
+    setCurrentClass: setCurrentClass,
+    classAssignments: classAssignments,
+    setClassAssignments: setClassAssignments,
     currentUser: currentUserInClass,
     isTeacher: currentUserInClass.role === USER_CLASS_ROLES.TEACHER,
   }
+
+  console.log(contextValue);
 
   return (
     <CurrentClassContext.Provider value={contextValue}>
---
+++ src/components/_pages/classroomSingle/context/helpers.js
@@ -0,0 +1,66 @@
+import { USER_CLASS_ROLES } from '../../../../helpers/constants';
+
+export const DEFAULT_USER_CLASS_ROLE = USER_CLASS_ROLES.STUDENT;
+
+export const USER_INITIAL_STATE = {
+  user_id: '',
+  user_code: '',
+  full_name: '',
+  role: DEFAULT_USER_CLASS_ROLE,
+}
+
+export const CLASS_INITIAL_STATE = {
+  class_id: '',
+  class_name: '',
+  subject: '',
+  description: '',
+  created_at: '',
+  updated_at: '',
+  status: '',
+  users: [],
+  owner: {
+    user_id: '',
+    avatar: '',
+    full_name: '',
+  }
+}
+
+export const CLASS_ASSIGNMENTS_INITIAL_STATE = [];
+
+export const getClassInfo = (classInfo) => {
+  const usersInClass = classInfo.listUser || [];
+
+  return {
+    class_id: classInfo.id || '',
+    class_name: classInfo.class_name || '',
+    subject: classInfo.subject || '',
+    description: classInfo.description || '',
+    created_at: classInfo.created_at || '',
+    updated_at: classInfo.updated_at || '',
+    status: classInfo.status || '',
+    users: usersInClass,
+    owner: {
+      user_id: classInfo.owner_id || '',
+      avatar: classInfo.owner_avatar || '',
+      full_name: classInfo.owner_name || '',
+    }
+  }
+}
+
+export const getClassAssignments = (classInfo) => {
+  const assignments = classInfo.listAssignments || [];
+
+  return assignments;
+}
+
+export const getCurrentUserInfoInClass = (usersInClass, currentUser) => {
+  const currentUsersInClass = usersInClass.filter((singleUser) => singleUser.user_id === currentUser.id)
+  let currentUserInClass = (currentUsersInClass.length > 0) ? currentUsersInClass[0] : USER_INITIAL_STATE;
+
+  return {
+    user_id: currentUserInClass.user_id,
+    user_code: currentUserInClass.user_code || '',
+    full_name: currentUserInClass.full_name || '',
+    role: currentUserInClass.role || DEFAULT_USER_CLASS_ROLE,
+  }
+}

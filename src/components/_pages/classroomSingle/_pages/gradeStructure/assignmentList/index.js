306e6bff9444f8a04cdd4ed9acd14c3ce0026c54
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-11-26 10:58:18 +0700

    Add stub re order button

--- src/components/_pages/classroomSingle/_pages/gradeStructure/assignmentList/index.js
+++ src/components/_pages/classroomSingle/_pages/gradeStructure/assignmentList/index.js
@@ -1,16 +1,32 @@
-import { Stack } from '@mui/material';
+import { 
+  Stack, 
+  Box, Paper, Typography, Button
+} from '@mui/material';
+
+import { AlignCenter } from '../../../../../_common/utilBoxes';
+
 import { useContext } from 'react';
 import { CurrentClassContext } from '../../../context/currentClassContext';
 import AssigmentListItem from './assignmentListItem';
 
 export default function AssignmentList() {
   const { classAssignments } = useContext(CurrentClassContext);
-  console.log(classAssignments)
+
   return (
-    <Stack my={2} spacing={1}>
-      {classAssignments.map((assignment) => (
-        <AssigmentListItem key={assignment.id} assignment={assignment}/>
-      ))}
-    </Stack>
+    <Box marginTop={2}>
+      <Paper>
+        <Box padding={1}>
+          <AlignCenter display='flex' >
+            <Typography sx={{flexGrow: 1}}>Cấu trúc điểm</Typography>
+            <Button variant='contained'>Lưu vị trí</Button>
+          </AlignCenter>
+        </Box>
+      </Paper>
+      <Stack my={2} spacing={1}>
+        {classAssignments.map((assignment) => (
+          <AssigmentListItem key={assignment.id} assignment={assignment}/>
+        ))}
+      </Stack>
+    </Box>
   )
 }
--- src/components/_pages/classroomSingle/_pages/gradeStructure/index.js
+++ src/components/_pages/classroomSingle/_pages/gradeStructure/index.js
@@ -10,7 +10,7 @@ export default function ClassroomGradeStructure() {
   return (
     <Container maxWidth='md'>
       <Box margin={2}>
-        <Typography variant='h6'>Chỉnh sửa cấu trúc điểm</Typography>
+        <Typography variant='h6'>Cấu trúc điểm</Typography>
       </Box>
       <CreateGradeForm/>
       <AssigmentList/>

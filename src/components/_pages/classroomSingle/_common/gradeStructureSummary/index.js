329a686181761d2eb999fbdc793ba68a8e217f5a
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-11-29 11:23:56 +0700

    Switch chart library

--- src/components/_pages/classroomSingle/_common/gradeStructureSummary/index.js
+++ src/components/_pages/classroomSingle/_common/gradeStructureSummary/index.js
@@ -1,35 +1,57 @@
-import { VictoryPie, VictoryContainer } from 'victory';
+import { Pie } from 'react-chartjs-2';
+import { Chart, ArcElement, Legend, Tooltip} from 'chart.js';
 import { Box, Paper, Typography, useTheme } from '@mui/material';
+import { cyan } from '@mui/material/colors';
 
 import { AlignCenter } from '../../../../_common/utilBoxes';
 
 import { useContext } from 'react';
 import { CurrentClassContext } from '../../context/currentClassContext';
 
+Chart.register(ArcElement);
+Chart.register(Legend);
+Chart.register(Tooltip);
+
+const colors = [
+  900, 800, 700, 600, 500, 400, 300
+].map((colorValue) => cyan[colorValue]);
+
 export default function GradeStructureSummary({title='Cấu trúc điểm'}) {
   const theme = useTheme();
   const { classAssignments } = useContext(CurrentClassContext);
+  const classAssignmentNames = classAssignments.map((assignment) => assignment.title);
+  const classAssignmentWeights = classAssignments.map((assignment) => assignment.weight);
 
-  const getLabelPosition = ({innerRadius, radius}) => {
-    return (innerRadius + radius) / 2;
+  const chartData = {
+    labels: classAssignmentNames,
+    datasets:[{
+      label: 'Phần trăm điểm',
+      data: classAssignmentWeights,
+      backgroundColor: colors,
+    }]
   }
+
+  const chartOptions = {
+    plugins: {
+      legend: { 
+        labels: {
+          color: theme.palette.text.primary
+        },
+        display: true,
+        position: 'top' 
+      },
+    },
+  };
 
   return (
     <Paper>
       <Box padding={2}>
         <Typography variant='h6'>{title}</Typography>
         <AlignCenter flexDirection='column'>
-          <Box maxWidth={300} maxHeight={300} overflow='visible'>
-            <VictoryPie
-              data={classAssignments}
-              colorScale='cool'
-              innerRadius={50}
-              labelRadius={getLabelPosition}
-              labels={({ datum }) => `${datum.title}: ${datum.weight}`}
-              style={{ labels: { fill: theme.palette.text.primary}}}
-              containerComponent={<VictoryContainer responsive={true}/>}
-              x='title'
-              y='weight'
+          <Box overflow='visible'>
+            <Pie
+              data={chartData}
+              options={chartOptions}
             />
           </Box>
         </AlignCenter>

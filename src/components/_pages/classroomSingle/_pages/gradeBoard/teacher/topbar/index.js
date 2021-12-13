a7b1edf351b8318aa318906eec2aa508ecca018c
Lê Nguyên Hào <47709398+lnhow@users.noreply.github.com>
2021-12-13 17:51:02 +0700

    Add stub template menu

--- src/components/_pages/classroomSingle/_pages/gradeBoard/teacher/topbar/index.js
+++ src/components/_pages/classroomSingle/_pages/gradeBoard/teacher/topbar/index.js
@@ -2,11 +2,15 @@ import {
   Typography,
   Box
 } from '@mui/material';
+import TemplateMenu from './templateMenu';
 
 export default function Topbar() {
   return (
-    <Box>
+    <Box display='flex' justifyContent='space-between'>
       <Typography variant='h6'>Điểm của lớp</Typography>
+      <Box>
+        <TemplateMenu/>
+      </Box>
     </Box>
   )
 }
